376f0bcbd192857bd23ea0e16fe913438a3cdf18
nxzpa007 <zpa007@gmail.com>
2022-04-27 09:56:47 +0900

    2204270956

--- src/App.js
+++ src/App.js
@@ -25,23 +25,26 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
 
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => {
-              //console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                />
-              );
-            })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, title, year, summary, poster }) {
-  return <h1>{title}</h1>;
+function Movie({ title, year, summary, poster }) {
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title" style={{ backgroundColor: "red" }}>
+          {title}
+        </h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.prototypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
